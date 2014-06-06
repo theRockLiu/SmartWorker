211f74d5b560c217dcd4dcb6ce9e25a60cc45a69
rock <rock@localhost.localdomain>
2014-06-06 15:46:06 +0800

    fd

--- main.go
+++ main.go
@@ -2,63 +2,73 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
+	"io"
+	"log"
 	"net"
+	"runtime"
+	"strings"
+	"unsafe"
 )
 
 type IBaseHandler interface {
-	Handle(*net.Conn)
+	HandleMsg(*net.Conn, uint32) error
 }
 
-var mapIdVsHandler map[int32]IBaseHandler
+type SMyHandler struct {
+	i int
+}
+
+func (this SMyHandler) HandleMsg(conn *net.Conn, ui32BodyLen uint32) error {
+	log.Println("SMyHandler HandleMsg: ", ui32BodyLen)
+
+	return nil
+}
+
+var mapIdVsHandler map[uint16]IBaseHandler
+
+type SMsgHeader struct {
+	len uint32
+	id  uint16
+	res uint16
+}
+
+var smh SMsgHeader
+
+const CONST_MSG_HEADER_LEN = unsafe.Sizeof(smh)
+
+const (
+	CONST_MSG_ID_MYTEST = 1
+)
+
+func init() {
+	mapIdVsHandler = make(map[uint16]IBaseHandler)
+	mapIdVsHandler[CONST_MSG_ID_MYTEST] = *(new(SMyHandler))
+}
 
 func main() {
-	var httpListener net.Listener
-	//var httpsListener net.Listener
-	mapIdVsHandler = make(map[int32]IBaseHandler)
 
-	context := &context{n}
+	go TcpService(string("0.0.0.0:9999"))
 
-	tcpListener, err := net.Listen("tcp", n.tcpAddr.String())
-	if err != nil {
-		log.Fatalf("FATAL: listen (%s) failed - %s", n.tcpAddr, err.Error())
-	}
-	n.tcpListener = tcpListener
-	tcpServer := &tcpServer{context: context}
-	n.waitGroup.Wrap(func() { util.TCPServer(n.tcpListener, tcpServer) })
-
-	//if n.tlsConfig != nil && n.httpsAddr != nil {
-	//	httpsListener, err = tls.Listen("tcp", n.httpsAddr.String(), n.tlsConfig)
-	//	if err != nil {
-	//		log.Fatalf("FATAL: listen (%s) failed - %s", n.httpsAddr, err.Error())
-	//	}
-	//	n.httpsListener = httpsListener
-	//	httpsServer := &httpServer{
-	//		context:     context,
-	//		tlsEnabled:  true,
-	//		tlsRequired: true,
-	//	}
-	//	n.waitGroup.Wrap(func() { util.HTTPServer(n.httpsListener, httpsServer, "HTTPS") })
+	//httpListener, err = net.Listen("tcp", strHttpListenAddr)
+	//if err != nil {
+	//	log.Fatalf("FATAL: listen (%s) failed - %s", n.httpAddr, err.Error())
 	//}
+	//go HttpService(string("0.0.0.0:8888"))
 
-	httpListener, err = net.Listen("tcp", n.httpAddr.String())
-	if err != nil {
-		log.Fatalf("FATAL: listen (%s) failed - %s", n.httpAddr, err.Error())
-	}
-	n.httpListener = httpListener
-	httpServer := &httpServer{
-		context:     context,
-		tlsEnabled:  false,
-		tlsRequired: n.options.TLSRequired,
-	}
-	n.waitGroup.Wrap(func() { util.HTTPServer(n.httpListener, httpServer, "HTTP") })
+	//waiting...
 }
 
-func TCPServer(listener net.Listener) {
-	log.Printf("TCP: listening on %s", listener.Addr().String())
+func TcpService(strListenAddr string) {
+	log.Printf("TCP: listening on %s", strListenAddr)
+
+	tcpListener, err := net.Listen("tcp", strListenAddr)
+	if err != nil {
+		log.Fatalf("FATAL: listen (%s) failed - %s", strListenAddr, err.Error())
+	}
 
 	for {
-		clientConn, err := listener.Accept()
+		clientConn, err := tcpListener.Accept()
 		if err != nil {
 			if nerr, ok := err.(net.Error); ok && nerr.Temporary() {
 				log.Printf("NOTICE: temporary Accept() failure - %s", err.Error())
@@ -71,10 +81,11 @@ func TCPServer(listener net.Listener) {
 			}
 			break
 		}
+
 		go HandleSession(clientConn)
 	}
 
-	log.Printf("TCP: closing %s", listener.Addr().String())
+	log.Printf("TCP: closing %s", tcpListener.Addr().String())
 }
 
 //func HTTPServer(listener net.Listener) {
@@ -95,35 +106,47 @@ func TCPServer(listener net.Listener) {
 func HandleSession(clientConn net.Conn) {
 	log.Printf("TCP: new client(%s)", clientConn.RemoteAddr())
 
-	// The client should initialize itself by sending a 4 byte sequence indicating
-	// the version of the protocol that it intends to communicate, this will allow us
-	// to gracefully upgrade the protocol away from text/line oriented to whatever...
-	buf := make([]byte, 4)
-	_, err := io.ReadFull(clientConn, buf)
-	if err != nil {
-		log.Printf("ERROR: failed to read protocol version - %s", err.Error())
-		return
-	}
-	int32(binary.BigEndian.Uint32(buf))
+	for {
+		// The client should initialize itself by sending a 4 byte sequence indicating
+		// the version of the protocol that it intends to communicate, this will allow us
+		// to gracefully upgrade the protocol away from text/line oriented to whatever...
+		bytesReadBuf := make([]byte, CONST_MSG_HEADER_LEN)
+		_, err := io.ReadFull(clientConn, bytesReadBuf)
+		if err != nil {
+			log.Printf("ERROR: failed to read protocol version - %s", err.Error())
+			return
+		}
 
-	log.Printf("CLIENT(%s): desired protocol magic '%s'", clientConn.RemoteAddr(), protocolMagic)
+		msgHdr := (*SMsgHeader)(unsafe.Pointer(&bytesReadBuf))
 
-	var prot util.Protocol
-	switch protocolMagic {
-	case "  V2":
-		prot = &protocolV2{context: p.context}
-	default:
-		util.SendFramedResponse(clientConn, frameTypeError, []byte("E_BAD_PROTOCOL"))
-		clientConn.Close()
-		log.Printf("ERROR: client(%s) bad protocol magic '%s'", clientConn.RemoteAddr(), protocolMagic)
-		return
-	}
+		if handler, OK := mapIdVsHandler[msgHdr.id]; OK {
+			handler.HandleMsg(&clientConn, msgHdr.len)
 
-	err = prot.IOLoop(clientConn)
-	if err != nil {
-		log.Printf("ERROR: client(%s) - %s", clientConn.RemoteAddr(), err.Error())
-		return
+		} else {
+			log.Fatalln("bad error!")
+			return
+		}
 	}
+	//int32(binary.BigEndian.Uint32(buf))
+
+	//log.Printf("CLIENT(%s): desired protocol magic '%s'", clientConn.RemoteAddr(), protocolMagic)
+
+	//var prot util.Protocol
+	//switch protocolMagic {
+	//case "  V2":
+	//	prot = &protocolV2{context: p.context}
+	//default:
+	//	util.SendFramedResponse(clientConn, frameTypeError, []byte("E_BAD_PROTOCOL"))
+	//	clientConn.Close()
+	//	log.Printf("ERROR: client(%s) bad protocol magic '%s'", clientConn.RemoteAddr(), protocolMagic)
+	//	return
+	//}
+
+	//err = prot.IOLoop(clientConn)
+	//if err != nil {
+	//	log.Printf("ERROR: client(%s) - %s", clientConn.RemoteAddr(), err.Error())
+	//	return
+	//}
 }
 
 // SendFramedResponse is a server side utility function to prefix data with a length header
@@ -148,43 +171,43 @@ func SendFramedResponse(w io.Writer, frameType int32, data []byte) (int, error) 
 	return n + 8, err
 }
 
-func readMPUB(r io.Reader, tmp []byte, idChan chan MessageID, maxMessageSize int64) ([]*Message, error) {
-	numMessages, err := readLen(r, tmp)
-	if err != nil {
-		return nil, util.NewFatalClientErr(err, "E_BAD_BODY", "MPUB failed to read message count")
-	}
+//func readMPUB(r io.Reader, tmp []byte, idChan chan MessageID, maxMessageSize int64) ([]*Message, error) {
+//	numMessages, err := readLen(r, tmp)
+//	if err != nil {
+//		return nil, util.NewFatalClientErr(err, "E_BAD_BODY", "MPUB failed to read message count")
+//	}
 
-	if numMessages <= 0 {
-		return nil, util.NewFatalClientErr(err, "E_BAD_BODY",
-			fmt.Sprintf("MPUB invalid message count %d", numMessages))
-	}
+//	if numMessages <= 0 {
+//		return nil, util.NewFatalClientErr(err, "E_BAD_BODY",
+//			fmt.Sprintf("MPUB invalid message count %d", numMessages))
+//	}
 
-	messages := make([]*Message, 0, numMessages)
-	for i := int32(0); i < numMessages; i++ {
-		messageSize, err := readLen(r, tmp)
-		if err != nil {
-			return nil, util.NewFatalClientErr(err, "E_BAD_MESSAGE",
-				fmt.Sprintf("MPUB failed to read message(%d) body size", i))
-		}
+//	messages := make([]*Message, 0, numMessages)
+//	for i := int32(0); i < numMessages; i++ {
+//		messageSize, err := readLen(r, tmp)
+//		if err != nil {
+//			return nil, util.NewFatalClientErr(err, "E_BAD_MESSAGE",
+//				fmt.Sprintf("MPUB failed to read message(%d) body size", i))
+//		}
 
-		if messageSize <= 0 {
-			return nil, util.NewFatalClientErr(nil, "E_BAD_MESSAGE",
-				fmt.Sprintf("MPUB invalid message(%d) body size %d", i, messageSize))
-		}
+//		if messageSize <= 0 {
+//			return nil, util.NewFatalClientErr(nil, "E_BAD_MESSAGE",
+//				fmt.Sprintf("MPUB invalid message(%d) body size %d", i, messageSize))
+//		}
 
-		if int64(messageSize) > maxMessageSize {
-			return nil, util.NewFatalClientErr(nil, "E_BAD_MESSAGE",
-				fmt.Sprintf("MPUB message too big %d > %d", messageSize, maxMessageSize))
-		}
+//		if int64(messageSize) > maxMessageSize {
+//			return nil, util.NewFatalClientErr(nil, "E_BAD_MESSAGE",
+//				fmt.Sprintf("MPUB message too big %d > %d", messageSize, maxMessageSize))
+//		}
 
-		msgBody := make([]byte, messageSize)
-		_, err = io.ReadFull(r, msgBody)
-		if err != nil {
-			return nil, util.NewFatalClientErr(err, "E_BAD_MESSAGE", "MPUB failed to read message body")
-		}
+//		msgBody := make([]byte, messageSize)
+//		_, err = io.ReadFull(r, msgBody)
+//		if err != nil {
+//			return nil, util.NewFatalClientErr(err, "E_BAD_MESSAGE", "MPUB failed to read message body")
+//		}
 
-		messages = append(messages, NewMessage(<-idChan, msgBody))
-	}
+//		messages = append(messages, NewMessage(<-idChan, msgBody))
+//	}
 
-	return messages, nil
-}
+//	return messages, nil
+//}
